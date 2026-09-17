4e023f72eb820e8ecd11a2136783538f39654d02
Joe3089 <146796577+Joe3089@users.noreply.github.com>
2026-09-17 13:56:29 -0400

    Add files via upload

---
+++ src/src/pages/Register/Register.jsx
@@ -0,0 +1,18 @@
+import "./Register.css";
+import RegisterForm from "../../components/Forms/RegisterForm/RegisterForm"
+
+function Register() {
+  return (
+    <div>
+      <div className="contenedor-register-page">
+        <div className="register-left">
+     </div>
+        <div className="register-rigth">
+          <RegisterForm />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
---
+++ src/src/pages/Servicios/Service3.jsx
@@ -0,0 +1,27 @@
+import Navbar from "../../components/Navbar/Navbar";
+import styles from "./Services.module.css";
+import imagen from "../../assets/img/3service3.jpeg";
+import Footer from "../../components/Footer/Footer";
+
+function Service3() {
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <h1>Retiros en efectivo</h1>
+        </div>
+        <img src={imagen} className={styles.img}></img>
+        <p className={styles.parrafo}>
+          El Banco Universitario también permite a los estudiantes realizar
+          retiros en efectivo de manera rápida y segura. Los estudiantes pueden
+          realizar retiros en cualquiera de las sucursales del banco, con la
+          tranquilidad de que sus fondos están seguros y protegidos.
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Service3;
